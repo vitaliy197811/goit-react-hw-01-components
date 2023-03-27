4b7363bed85238d682c59b63d053532ab32a23cc
vitaliy197811 <vital197811@gmail.com>
2023-03-27 13:15:01 +0300

    refactoring

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,6 @@ export const Statistics = ({ title, stats }) => {
     return( 
         <section className={css.statistics}>
             {title && <h2 className={css.title}>{title}</h2>}
-        
             <ul className={css.stat_list}>
                 {stats.map(({ id, label, percentage }) => (
                     <li className={css.item} key={id}>
